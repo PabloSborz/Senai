a28586297a362723a6552adb0b1d981a49506b4f
Pablo Sborz <linkpablosborz123@gmail.com>
2026-06-12 16:26:44 -0300

    commit -m "feat: add pablo-link component and update app.html and angular.json"

---
+++ Angular/pablo-sborz/src/app/pablo-link/pablo-link.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pablo-link',
+  imports: [],
+  templateUrl: './pablo-link.html',
+  styleUrl: './pablo-link.css',
+})
+export class PabloLink {}
