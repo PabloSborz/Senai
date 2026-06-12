852aaacf9a2098d068988e612c6f2c55587c5d96
Pablo Sborz <linkpablosborz123@gmail.com>
2026-06-12 17:09:36 -0300

    commit -m "Alterações"

--- Angular/pablo-sborz/src/app/app.ts
+++ Angular/pablo-sborz/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PabloLink } from "./pablo-link/pablo-link";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PabloLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
