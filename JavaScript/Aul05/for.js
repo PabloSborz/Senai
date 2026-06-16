c4c086f5e592b65ab1da89a7c1cb26095ba820b9
Pablo Sborz <linkpablosborz123@gmail.com>
2026-06-16 17:21:19 -0300

    commit -m "Alteracao "

---
+++ JavaScript/Aul05/for.js
@@ -0,0 +1,42 @@
+// for(i = 0; i < 5; i++){
+//     alert("Bom dia!")
+// }
+
+// let soma = 0
+// for(i = 0; i < 5; i++){
+//     soma += 2;
+//     alert(soma);
+// }
+
+// let soma = 0
+// for (i = 0; i <= 19; i++){
+//     if(i % 2 == 0){
+//         continue
+//     }else{
+//         alert("numero impar:" + i)
+//     }
+// }
+
+// let soma = 0
+// for (i = 0; i <= 30; i++){
+//     if(i % 2 == 0){
+//         alert("numero par:" + i)
+//     }else{
+//         continue
+//     }
+// }
+
+// let soma = 0
+// for (i = 0; i <= 30; i++){
+//     if(i % 2 == 0){
+//         alert("numero par:" + i)
+//     }else{
+//         continue
+//     }
+// }
+
+let opcao = "nao"
+while(opcao != "sim"){
+    console.log("Ola")
+    opcao = prompt("Quer parar? 'sim' - para - 'nao' - continua")
+}
