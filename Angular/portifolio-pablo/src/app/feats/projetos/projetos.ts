cb2704a06f9f67dfd4207279150d37b3e102261f
PabloSborz <linkpablosborz123@gmail.com>
2026-07-05 00:09:34 -0300

    oPortifolio

--- Angular/portifolio-pablo/src/app/feats/projetos/projetos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-projetos',
-  imports: [],
-  templateUrl: './projetos.html',
-  styleUrl: './projetos.css',
-})
-export class Projetos {}
